046121614fa4dd8cfc863c6f95364bff2632c686
Gwenyfar <iruoma.onyia@cyphercrescent.com>
2024-02-20 08:05:12 +0100

    oauth beginning

---
+++ BookBinder.Idp/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookBinder.Idp.Pages.Account;
+
+public class AccessDeniedModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
--- BookBinder/Program.cs
+++ BookBinder/Program.cs
@@ -1,8 +1,10 @@
 using BookBinder.Application;
 using BookBinder.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.OpenApi.Models;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,11 +13,45 @@ var configuration = builder.Configuration;
 var appConfig = configuration.ExtractAppSettings(LoggerFactory.Create(l => l.AddConsole()));
 var bootstrapper = new Bootstrapper(appConfig);
 
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
+//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(o =>
+{
+    o.Authority = "https://localhost:5001";
+    o.TokenValidationParameters.ValidateAudience = false;
+    o.TokenValidationParameters.ValidTypes = new[] { "at+jwt" };
+});
 builder.Services.AddControllers(o=> o.Filters.Add(new AuthorizeFilter()));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            ClientCredentials = new OpenApiOAuthFlow
+            {
+                TokenUrl = new Uri("https://localhost:5001/connect/token"),
+                Scopes = new Dictionary<string, string>
+                {
+                    {"bookbinderapi", "Access to Bookbinder API" },
+                }
+            }
+        }
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "oauth2",
+                    Type = ReferenceType.SecurityScheme
+                }
+            }, new List<string>()
+        }
+    });
     var path = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     var pathString = Path.Combine(AppContext.BaseDirectory, path);
     options.IncludeXmlComments(pathString);
